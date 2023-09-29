560731a6c0ecb0e021abc03884afd952027b52ee
Adrienn G <62941853+Dricc06@users.noreply.github.com>
2023-09-29 14:47:58 +0200

    Create index.php
    Adatbázis kapcsolódás tesztelése

---
+++ src/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+    
+    echo 'Kapcsolat teszelésének kezdete...<br>';
+    require_once('./database.php');
+    $conn = get_connection(); 
+    echo 'Kapcsolat teszelésének vége...<br>';
+
+    // --------------------------------------------------------------------------
+
+    // Lekérdezések tesztelése
+?>   
