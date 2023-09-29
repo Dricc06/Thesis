3e759402f71694f208e3b4ade5feddcfab604844
Adrienn G <62941853+Dricc06@users.noreply.github.com>
2023-09-29 15:02:02 +0200

    Database connection for the teacher main page
    PHP adatbázis kapcsolat az oktatói főoldla részére, autentikációval.

---
+++ src/fooldal_oktato.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['user_type']) || $_SESSION['user_type'] != "1") {
+    // Ellenőrizze, hogy a felhasználó be van-e jelentkezve és oktató-e
+    header("Location: login.php");
+    exit();
+}
+
+// Adatbázis kapcsolat
+$servername = "localhost";
+$username = "Admin";
+$password = "_K*uqlR2qRzexuzw";
+$dbname = "SZD_jatekositas";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Kapcsolódási hiba: " . $conn->connect_error);
+}
+
+// Felhasználó adatainak lekérdezése
+$username= $_SESSION['username'];
+$sql = "SELECT avatar, neptun_kod FROM users WHERE neptun_kod = '$username'";
+$result = $conn->query($sql);
+
+if ($result->num_rows == 1) {
+    $row = $result->fetch_assoc();
+    $avatar = $row['avatar']; // Avatar elérési útvonala az adatbázisból
+    $username = $row['neptun_kod']; // Neptun kód az adatbázisból
+}
+
+$conn->close();
+?>
