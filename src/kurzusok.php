69cbf68cc0cdc7db4dd3253fafd1c9f673cf90f9
Adrienn G <62941853+Dricc06@users.noreply.github.com>
2023-09-30 14:36:09 +0200

    Created the PHP base
    Autentikáció, oktatói adatok lekérése, oktaóhoz tartozó kurzusok lekérése.

---
+++ src/kurzusok.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['user_type']) || $_SESSION['user_type'] != "1") {
+    // Ellenőrizze, hogy a felhasználó be van-e jelentkezve és oktató-e
+    header("Location: login.php");
+    exit();
+}
+
+// Adatbázis kapcsolat
+$servername = "localhost";
+$username = "Admin";
+$password = "_K*uqlR2qRzexuzw";
+$dbname = "SZD_jatekositas";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Kapcsolódási hiba: " . $conn->connect_error);
+}
+
+// Felhasználó adatainak lekérdezése
+$username= $_SESSION['username'];
+$sql = "SELECT avatar, neptun_kod FROM users WHERE neptun_kod = '$username'";
+$result = $conn->query($sql);
+
+if ($result->num_rows == 1) {
+    $row = $result->fetch_assoc();
+    $avatar = $row['avatar']; // Avatar elérési útvonala az adatbázisból
+    $username = $row['neptun_kod']; // Neptun kód az adatbázisból
+}
+
+// Oktatóhoz tartozó kurzusok lekérése
+$oktatoKod = $_SESSION['username']; // Az oktató neptun kódja
+
+$sql = "SELECT kurzusnev FROM kurzus WHERE koktato = '$oktatoKod'";
+$result = $conn->query($sql);
+
+$oktatoKurzusok = array();
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $oktatoKurzusok[] = $row['kurzusnev'];
+    }
+}
+
+$conn->close();
+?>
