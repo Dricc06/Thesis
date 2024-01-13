b377347084f584582ea2fd851a1990fcfff9c621
Adrienn G <62941853+Dricc06@users.noreply.github.com>
2024-01-13 23:57:02 +0100

    Jelszó titkosítás hozzáadása

--- src/login.php
+++ src/login.php
@@ -67,30 +67,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST["password"];
 
     // Ellenőrzés az adatbázisban
-    $sql = "SELECT * FROM users WHERE neptun_kod = '$username' AND jelszo = '$password' AND user_type = '$userType'";
+    $sql = "SELECT * FROM users WHERE neptun_kod = '$username' AND user_type = '$userType'";
     $result = $conn->query($sql);
 
     if ($result->num_rows == 1) {
-        // Sikeres belépés
-        session_start();
-        $_SESSION["username"] = $username;
-        $_SESSION["user_type"] = $userType;
+        $row = $result->fetch_assoc();
+        $hashedPasswordFromDatabase = $row['jelszo'];
 
-        // Átirányítás a megfelelő céloldalra
-        if ($userType == "2") {
-            header("Location: fooldal_hallgato.php");
-        } elseif ($userType == "1") {
-            header("Location: fooldal_oktato.php");
+        // Beírt és eltárolt titkosított jelszó összehasonlítása
+        if (hash_equals($hashedPasswordFromDatabase, md5($password))) {
+            $_SESSION["username"] = $username;
+            $_SESSION["user_type"] = $userType;
+
+            // Átirányítás a megfelelő céloldalra
+            if ($userType == "2") {
+                header("Location: fooldal_hallgato.php");
+            } elseif ($userType == "1") {
+                header("Location: fooldal_oktato.php");
+            }
+
+            exit();
         }
-
-        exit();
-    } else {
-        // Belépés sikertelen
-        $_SESSION['error'] = "Hibás felhasználónév, jelszó vagy belépési típus.";
-        header("Location: login.php"); // Átirányítás a bejelentkező oldalra
-        exit();
     }
+
+    // Belépés sikertelen
+    $_SESSION['error'] = "Hibás felhasználónév, jelszó vagy belépési típus.";
+    header("Location: login.php"); // Átirányítás a bejelentkező oldalra
+    exit();
 }
+
+
+
+
+
+
+
+
+
+
+
 
 $conn->close();
 ?>
